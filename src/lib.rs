f7f9c0d55ab171b0fc45a10ae8a24460617cd52e
Ho Kim <ho.kim@ulagbulag.io>
2022-05-12 23:50:55 +0000

    Expose ordered_float

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@ pub extern crate anyhow;
 pub extern crate ed25519_dalek;
 pub extern crate generic_array;
 pub extern crate ndarray;
+pub extern crate ordered_float;
 
 pub mod account;
 pub mod credit;
