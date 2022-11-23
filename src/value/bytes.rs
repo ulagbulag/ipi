4bb028ec2ae697b46f0eea3b486e5628c0eb3728
Ho Kim <ho.kim@ulagbulag.io>
2022-11-23 05:03:39 +0000

    Impl Deref for Bytes

--- src/value/bytes.rs
+++ src/value/bytes.rs
@@ -1,3 +1,5 @@
+use core::ops;
+
 use base58::ToBase58;
 use bytecheck::CheckBytes;
 use rkyv::{Archive, Deserialize, Serialize};
@@ -15,6 +17,20 @@ use rkyv::{Archive, Deserialize, Serialize};
 #[archive(compare(PartialEq))]
 #[archive_attr(derive(CheckBytes, Debug, PartialEq))]
 pub struct Bytes(pub Vec<u8>);
+
+impl ops::Deref for Bytes {
+    type Target = Vec<u8>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl ops::DerefMut for Bytes {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
 
 impl ToBase58 for Bytes {
     fn to_base58(&self) -> String {
